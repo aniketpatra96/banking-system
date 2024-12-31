59bd5669d903526a09768b81c729b27aa458c356
ANIKET PATRA <110764399+aniketpatra96@users.noreply.github.com>
2024-12-31 16:13:12 +0530

    added loan interest update

--- src/bank/acctypes/loanaccount/LoanAccount.java
+++ src/bank/acctypes/loanaccount/LoanAccount.java
@@ -3,7 +3,11 @@ package bank.acctypes.loanaccount;
 import bankdatabase.BankServerConnecter;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 public class LoanAccount {
     private long loanNo;
@@ -13,6 +17,48 @@ public class LoanAccount {
 
     public LoanAccount(long loanNo) {
         this.loanNo = loanNo;
+        if(checkLoanAccount(loanNo))
+            generateCompoundInterest();
+    }
+
+    private void generateCompoundInterest() {
+        String fetchQuery = "SELECT * FROM LOAN_ACCOUNT L NATURAL JOIN LOAN_TYPES T WHERE L.LOAN_TYPE = T.LOAN_TYPE AND LOAN_NO = " + loanNo;
+        double amount,interestRate,termPeriod,newBalance;
+        Timestamp t;
+        try {
+            con = BankServerConnecter.getDatabaseConnection();
+            psmt = con.prepareStatement(fetchQuery);
+            rs = psmt.executeQuery();
+            if(rs.next()){
+                amount = rs.getDouble("AMOUNT");
+                interestRate = rs.getDouble("INTEREST_RATE");
+                termPeriod = rs.getDouble("TERM_PERIOD");
+                t = rs.getTimestamp("LOAN_DATE");
+                long ms = t.getTime();
+                Date timestamp1 = new Date();
+                Date timestamp2 = new Date(ms);
+                LocalDate date1 = timestamp1.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                LocalDate date2 = timestamp2.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                long yearsWithdrawn = ChronoUnit.YEARS.between(date2, date1);
+                if(yearsWithdrawn >= 1 && yearsWithdrawn <= termPeriod) {
+                    newBalance = amount * Math.pow((1 + interestRate), yearsWithdrawn);
+                    psmt = con.prepareStatement("UPDATE LOAN_ACCOUNT SET AMOUNT = ? WHERE LOAN_NO = ?");
+                    psmt.setDouble(1,newBalance);
+                    psmt.setLong(2,loanNo);
+                    psmt.executeUpdate();
+                }
+            }
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                rs.close();
+                psmt.close();
+                con.close();
+            }catch(SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static boolean checkLoanAccount(long laccNo) {
@@ -36,8 +82,6 @@ public class LoanAccount {
             }
         }
     }
-
-
 
     public void getLoanDetails() {
         double amount,termPeriod,interestRate;
